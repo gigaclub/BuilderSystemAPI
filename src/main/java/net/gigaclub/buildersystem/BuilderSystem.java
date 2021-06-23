f3f30ad717479dbbbcfb1ebc5703fd75d9c4e228
Kevin <kevin@gigaclub.net>
2021-06-23 21:55:54 +0200

    imp(BuilderSystemAPI): added more functions

--- src/main/java/net/gigaclub/buildersystem/BuilderSystem.java
+++ src/main/java/net/gigaclub/buildersystem/BuilderSystem.java
@@ -23,7 +23,7 @@ public class BuilderSystem {
         try {
             return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
                     this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
-                    "gc.team", "create_team", Arrays.asList(playerUUID, name)
+                    "gc.builder.team", "create_team", Arrays.asList(playerUUID, name)
             ));
         } catch (XmlRpcException e) {
             e.printStackTrace();
@@ -41,7 +41,7 @@ public class BuilderSystem {
         try {
             return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
                     this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
-                    "gc.team", "create_team", Arrays.asList(playerUUID, name, description)
+                    "gc.builder.team", "create_team", Arrays.asList(playerUUID, name, description)
             ));
         } catch (XmlRpcException e) {
             e.printStackTrace();
@@ -60,7 +60,7 @@ public class BuilderSystem {
         try {
             return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
                     this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
-                    "gc.team", "edit_team", Arrays.asList(playerUUID, name, newName)
+                    "gc.builder.team", "edit_team", Arrays.asList(playerUUID, name, newName)
             ));
         } catch (XmlRpcException e) {
             e.printStackTrace();
@@ -79,7 +79,7 @@ public class BuilderSystem {
         try {
             return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
                     this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
-                    "gc.team", "edit_team", Arrays.asList(playerUUID, name, newName, newDescription)
+                    "gc.builder.team", "edit_team", Arrays.asList(playerUUID, name, newName, newDescription)
             ));
         } catch (XmlRpcException e) {
             e.printStackTrace();
@@ -87,8 +87,78 @@ public class BuilderSystem {
         return 5;
     }
 
+    // Status Codes:
+    // 2: Other error
+    // 1: User has no team
+    // 0: Success
     public int leaveTeam(String playerUUID) {
-        
+        try {
+            return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
+                    "gc.builder.team", "leave_team", Arrays.asList(playerUUID)
+            ));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+        return 2;
+    }
+
+    // Status Codes:
+    // 4: Other error
+    // 3: Team does not exist
+    // 2: User is not manager
+    // 1: User is not user of this team
+    // 0: Success
+    public int kickMember(String playerUUID, String playerUUIDtoKick) {
+        try {
+            return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
+                    "gc.builder.team", "kick_member", Arrays.asList(playerUUID, playerUUIDtoKick)
+            ));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+        return 4;
+    }
+
+    // Status Codes:
+    // 6: Other error
+    // 5: Team does not exist
+    // 4: User is not manager
+    // 3: User to kick is not a team
+    // 2: User to kick is not in this team
+    // 1: User is already manager of this team
+    // 0: Success
+    public int promoteMember(String playerUUID, String playerUUIDtoPromote) {
+        try {
+            return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
+                    "gc.builder.team", "promote_member", Arrays.asList(playerUUID, playerUUIDtoPromote)
+            ));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+        return 6;
+    }
+
+    // Status Codes:
+    // 6: Other error
+    // 5: Team does not exist
+    // 4: User is not manager
+    // 3: User to kick is not a team
+    // 2: User to kick is not in this team
+    // 1: User is already user of this team
+    // 0: Success
+    public int demoteMember(String playerUUID, String playerUUIDtoDemote) {
+        try {
+            return (int) this.odoo.getModels().execute("execute_kw", Arrays.asList(
+                    this.odoo.getDatabase(), this.odoo.getUid(), this.odoo.getPassword(),
+                    "gc.builder.team", "demote_member", Arrays.asList(playerUUID, playerUUIDtoDemote)
+            ));
+        } catch (XmlRpcException e) {
+            e.printStackTrace();
+        }
+        return 6;
     }
 
 }
