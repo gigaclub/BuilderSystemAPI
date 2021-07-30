998263a8b4bd94871bbc852552213104714dc3c2
Kevin <kevin@gigaclub.net>
2021-07-30 20:39:54 +0200

    imp(BuilderSystemAPI): updated to 14.0.1.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "net.gigaclub"
-version = "14.0.1.0.3"
+version = "14.0.1.0.4"
 
 val myArtifactId: String = rootProject.name
 val myArtifactGroup: String = project.group.toString()
