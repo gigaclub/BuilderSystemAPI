c40dffa30550da5eb0506ba7e0eb88b9ebb75273
Kevin <kevin@gigaclub.net>
2021-08-01 16:12:27 +0200

    fix(BuilderSystemAPI): fixed removeTask and updated version (#20)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "net.gigaclub"
-version = "14.0.1.0.4"
+version = "14.0.1.0.5"
 
 val myArtifactId: String = rootProject.name
 val myArtifactGroup: String = project.group.toString()
